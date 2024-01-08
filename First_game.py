fe85ead9d4c222dca8e13eda174f58405bfcc95f
NhuNhui <giaqui1403@gmail.com>
2024-01-09 02:55:47 +0700

    add bird

--- First_game.py
+++ First_game.py
@@ -21,16 +21,39 @@ x_floor = 0
 #create screen game
 screen = pygame.display.set_mode((431,768))
 
+#game object bird
+bird = pygame.image.load(r'FileGame\assets\yellowbird-midflap.png')
+bird = pygame.transform.scale2x(bird)
+
+bird_rectangle = bird.get_rect(center=(100,768/2))
+
+#trọng lực P
+P = 0.1
+y_bird = 0
+
+
 #game loop
 run = True
 while run:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             run = False
+        if(event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_SPACE):
+                y_bird = -10   
     
     screen.blit(bg,(0,0))
     
     #run floor
     x_floor -= 1
     screen.blit(floor,(x_floor,0+600))
+    screen.blit(floor,(x_floor + 432,0+600))
+    if(x_floor <= -432):
+        x_floor = 0
+        
+    #add bird in game
+    screen.blit(bird,bird_rectangle) 
+    y_bird += P
+    bird_rectangle.centery += y_bird   
+        
     pygame.display.update()
